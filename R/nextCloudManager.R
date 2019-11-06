3e3433b96f453021ce6deec961580d85cb631b4d
eblondel <emmanuel.blondel1@gmail.com>
2019-11-06 09:04:38 +0100

    #1 version/capabilities

--- R/nextCloudManager.R
+++ R/nextCloudManager.R
@@ -15,6 +15,15 @@
 #'  \item{\code{connect()}}{
 #'    A method to connect to 'Nextcloud' 
 #'  }
+#'  \item{\code{getVersion()}}{
+#'    Get the 'ownCloud' server version
+#'  }
+#'  \item{\code{getCapabilities()}}{
+#'    Get the 'ownCloud' server capabilities
+#'  }
+#'  \item{\code{getWebdavRoot()}}{
+#'    Get the 'ownCloud' WebDAV root URL
+#'  }
 #' }
 #' 
 #' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
@@ -24,7 +33,9 @@ nextCloudManager <-  R6Class("nextCloudManager",
   private = list(
     url = NULL,
     user = NULL,
-    pwd = NULL
+    pwd = NULL,
+    version = NULL,
+    capabilities = NULL
   ),
   public = list(
     
@@ -38,7 +49,37 @@ nextCloudManager <-  R6Class("nextCloudManager",
     
     #connect
     connect = function(){
-     stop("Not yet implemented")
+      caps_req <- nextcloudRequest$new(
+        type = "HTTP_GET", private$url, "ocs/v1.php/cloud/capabilities",
+        private$user, private$pwd, logger = self$loggerType
+      )
+      caps_req$execute()
+      caps_resp <- caps_req$getResponse()
+      if(caps_resp$ocs$meta$status == "failure"){
+        errMsg <- sprintf("Could not connect to Nextcloud '%s': %s", private$url, caps_resp$ocs$meta$message)
+        self$ERROR(errMsg)
+        stop(errMsg)
+      }
+      if(caps_resp$ocs$meta$status == "ok"){
+        self$INFO(sprintf("Successful connection to Nextcloud '%s'!", private$url))
+        private$version = caps_resp$ocs$data$version
+        private$capabilities = caps_resp$ocs$data$capabilities
+      }
+    },
+    
+    #getVersion
+    getVersion = function(){
+      return(private$version)
+    },
+    
+    #getCapabilities
+    getCapabilities = function(){
+      return(private$capabilities)
+    },
+    
+    #getWebdavRoot
+    getWebdavRoot = function(){
+      return(private$capabilities$core[["webdav-root"]])
     }
   )
 )
